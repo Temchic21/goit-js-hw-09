1cd480bf68ec7c8846737c7cb267a344877f967a
Temchic21 <barsiccc@gmail.com>
2022-11-02 21:15:31 +0200

    commit-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+
+const backgroundColorСhange = function () {
+  body.style.backgroundColor = getRandomHexColor();
+  body.style.color = getRandomHexColor();
+};
+
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(backgroundColorСhange, 1000);
+  btnStart.disabled = true;
+});
+
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+});
